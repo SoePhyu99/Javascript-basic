2dbc5366ce9288ffba12b3671d4872d0648eadbc
Soe Phyu <soephyuphyuhtun99@gmail.com>
2023-06-09 14:33:08 +0630

    Max of two numbers.

---
+++ index.js
@@ -0,0 +1,17 @@
+// swapping variable
+let a = 'red'
+let b = 'blue'
+
+let c = a
+a = b
+b = c
+
+console.log(a);
+console.log(b);
+
+// max of two numbers
+function max(a, b) {
+    return a > b ? a : b;
+}
+console.log(max(4, 3));
+
